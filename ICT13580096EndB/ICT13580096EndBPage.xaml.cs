6af4abc8ce929cae5efebddfdbf0e9b67aa525f2
theerakhanthiyu <easywritell@gmail.com>
2017-12-21 17:29:46 +0700

    zu1

---
+++ ICT13580096EndB/ICT13580096EndBPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ICT13580096EndB
+{
+    public partial class ICT13580096EndBPage : ContentPage
+    {
+        public ICT13580096EndBPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
